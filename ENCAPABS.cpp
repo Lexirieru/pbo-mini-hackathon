6f9107a6b080708c5de70834f23c5fda64d80322
Ramzi Alfito Rizky <175444158+ZiramBayam@users.noreply.github.com>
2025-09-24 13:53:56 +0700

    Add files via upload
    Problem 2 Mini Hackathon OOP

---
+++ ENCAPABS.cpp
@@ -0,0 +1,53 @@
+// 24/540550/TK/60008
+//
+
+#include <iostream>
+using namespace std;
+
+class FuelTank {
+    private :
+    long long fuel_level;
+
+    public :
+    FuelTank(long long level){
+        fuel_level = level;
+    }
+
+    void refuel(long long x){
+        fuel_level += x;
+    }
+
+    void travel(long long x){
+        if(fuel_level >= x){
+            fuel_level -= x;
+        }
+    }
+
+    long long report() const {
+        return fuel_level;
+    }
+};
+
+int main(){
+    int initial_fuel;
+    int Q;
+
+    cin >> initial_fuel >> Q;
+
+    FuelTank tank(initial_fuel);
+    for (int run = 0; run < Q; ++run){
+        string command;
+        int x;
+        cin >> command >> x;
+        if (command == "REFUEL"){
+            tank.refuel(x);
+        } 
+        else if (command == "TRAVEL"){
+            tank.travel(x);
+        }
+        else if (command == "REPORT"){
+            cout << tank.report() << endl;
+        }
+    }
+    return 0;
+}
